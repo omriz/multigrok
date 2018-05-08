08cf4d12e0b7c5735cf746844bce562552dc083b
Omri Zohar <omriz@google.com>
2018-05-08 12:40:28 +0300

    Adding parallel execution for searching

--- middleware/searcher.go
+++ middleware/searcher.go
@@ -3,12 +3,12 @@ package middleware
 import (
 	"log"
 	"net/url"
+	"sync"
 
 	"github.com/omriz/multigrok/backends"
 )
 
-func Search(servers map[string]backends.Backend, qparams url.Values) map[string]backends.WebServiceResult {
-	results := make(map[string]backends.WebServiceResult)
+func parseParams(qparams url.Values) string {
 	if val, ok := qparams["q"]; ok {
 		qparams.Add("freetext", val[0])
 		qparams.Del("q")
@@ -25,13 +25,34 @@ func Search(servers map[string]backends.Backend, qparams url.Values) map[string]
 		// We intentionally drop the project.
 		qparams.Del("project")
 	}
+	return qparams.Encode()
+}
+
+func Search(servers map[string]backends.Backend, qparams url.Values) map[string]backends.WebServiceResult {
+	q := parseParams(qparams)
+	// Parallel execution over all backends.
+	var wg sync.WaitGroup
+	var m sync.Map
 	for name, backend := range servers {
-		res, err := backend.Query(qparams.Encode())
-		if err != nil {
-			log.Printf("Error returned from %s: %v\n", name, err)
-		} else {
-			// We only append the result if we have any.
-			results[name] = res
+		wg.Add(1)
+		go func(name string, backend backends.Backend, query string, m *sync.Map, wg *sync.WaitGroup) {
+			res, err := backend.Query(query)
+			if err != nil {
+				log.Printf("Error returned from %s: %v\n", name, err)
+			} else {
+				// We only append the result if we have any.
+				m.Store(name, res)
+			}
+			wg.Done()
+		}(name, backend, q, &m, &wg)
+	}
+	wg.Wait()
+	results := make(map[string]backends.WebServiceResult)
+	for name := range servers {
+		if v, ok := m.Load(name); ok {
+			if c, k := v.(backends.WebServiceResult); k {
+				results[name] = c
+			}
 		}
 	}
 	return results
